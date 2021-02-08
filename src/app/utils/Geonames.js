e73527c38858ac36767e049750b0aab7016bb9e9
Rahul Singh <dr@ph4nt0m.me>
2021-02-09 00:39:08 +0530

    [feature] search by location

--- src/app/utils/Geonames.js
+++ src/app/utils/Geonames.js
@@ -1,4 +1,5 @@
 const axios = require('axios').default;
+const services = require('../../config/services');
 const logger = require('./Logger');
 
 module.exports = class Geonames {
@@ -10,13 +11,34 @@ module.exports = class Geonames {
   static async getTimezone(lat, long) {
     return new Promise(async (resolve, reject) => {
       try {
-        const response = await this.api.get(`/timezoneJSON?formatted=true&lat=${lat}&lng=${long}&username=targaryen&style=full`);
+        const response = await this.api.get(`/timezoneJSON?formatted=true&username=${services.geonames.username}&lat=${lat}&lng=${long}&style=full`);
 
         if (response.status !== 200) {
           reject(new Error(`cannot retrieve timezone information`));
         }
 
         resolve(response.data);
+      } catch (error) {
+        logger.error(`[x] ${error}`);
+        reject(new Error(error.message || `internal server error`));
+      }
+    });
+  }
+
+  static async getCoordinates(location) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const response = await this.api.get(`/searchJSON?formatted=true&username=${services.geonames.username}&q=${location}&maxRows=1`);
+
+        if (response.status !== 200) {
+          reject(new Error(`cannot retrieve location information`));
+        }
+
+        if (response.data.geonames.length === 0) {
+          reject(new Error(`could not find the location ${location}`));
+        }
+
+        resolve({ latitude: response.data.geonames[0].lat, longitude: response.data.geonames[0].lng });
       } catch (error) {
         logger.error(`[x] ${error}`);
         reject(new Error(error.message || `internal server error`));
