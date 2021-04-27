254087906c9cd4ecc20726f97a07f9c14b4fc4eb
Rahul Singh <dr@ph4nt0m.com>
2021-04-27 22:39:02 +0530

    shard spawn fix

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 require('../config/setup');
 require('../config/sentry');
-require('./server');
+// require('./server');
 
 const { ShardingManager } = require('discord.js');
 const services = require('../config/services');
@@ -8,11 +8,11 @@ const logger = require('./utils/Logger');
 
 const manager = new ShardingManager('src/app/bot.js', {
   totalShards: 'auto',
-  token: services.discord.token
+  token: services.discord.token,
 });
 
 manager.on('shardCreate', (shard) => {
   logger.info(`Launched shard ${shard.id}`);
 });
 
-manager.spawn();
+manager.spawn(3, 1000, -1);
