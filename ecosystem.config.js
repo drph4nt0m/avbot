1354b971380648ef377bb62cfb5010527bb496c4
Rahul Singh <rahul.singh12221998@gmail.com>
2021-09-02 17:07:25 +0530

    updates pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'avbot',
-      script: 'npm',
-      args: 'start'
+      script: 'node',
+      args: './src/app/index.js',
+      watch: ['src'],
+      watch_delay: 1000,
+      ignore_watch: ['node_modules', 'tmp', '*.log']
     }
   ]
 };
