464141212fe4446716ac4f1dce49eb768320d16e
Weeby <loli@victorique.moe>
2022-04-25 00:35:41 +0100

    Update Beans.ts
    remove unnecessary symbols

--- src/model/framework/DI/Beans.ts
+++ src/model/framework/DI/Beans.ts
@@ -1,6 +1,3 @@
 export class Beans {
-    public static ISubModuleToken = Symbol("ISubModule");
-    public static ICloseableModuleToken = Symbol("ICloseableModule");
-    public static IPermissionEventListener = Symbol("IPermissionEventListener");
     public static IPropertyResolutionEngine = Symbol("IPropertyResolutionEngine");
 }
