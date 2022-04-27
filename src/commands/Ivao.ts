1d7af3d4fbd460221c2135616d7cb2add06d2737
VictoriqueMoe <loli@victorique.moe>
2022-04-27 20:58:11 +0100

    added search on IVAO

--- src/commands/Ivao.ts
+++ src/commands/Ivao.ts
@@ -1,12 +1,14 @@
 import {Category, NotBot} from "@discordx/utilities";
-import {CommandInteraction, MessageEmbed} from "discord.js";
+import {AutocompleteInteraction, CommandInteraction, MessageEmbed} from "discord.js";
 import {Client, Discord, Guard, Slash, SlashChoice, SlashOption} from "discordx";
 import {injectable} from "tsyringe";
 
 import {GuildOnly} from "../guards/GuildOnly.js";
 import {RequiredBotPerms} from "../guards/RequiredBotPerms.js";
 import {IvaoManager} from "../model/framework/manager/IvaoManager.js";
-import {IvaoPilot} from "../model/Typeings.js";
+import type {IvaoAtc, IvaoPilot} from "../model/Typeings.js";
+import logger from "../utils/LoggerFactory.js";
+import {InteractionUtils, ObjectUtil} from "../utils/Utils.js";
 
 @Discord()
 @Category("IVAO commands")
@@ -23,7 +25,7 @@ export class Ivao {
     @Guard(NotBot, RequiredBotPerms({
         textChannel: ["EMBED_LINKS"]
     }), GuildOnly)
-    private async atisVoice(
+    private atisVoice(
         @SlashChoice("atc", "pilot")
         @SlashOption("type", {
             // TODO: fill this info out
@@ -34,6 +36,7 @@ export class Ivao {
             type: "atc" | "pilot",
         @SlashOption("call-sign", {
             description: "What call sign would you like the bot to give information for?",
+            autocomplete: (interaction: AutocompleteInteraction) => InteractionUtils.search(interaction, IvaoManager),
             type: "STRING",
             required: true
         })
@@ -50,99 +53,166 @@ export class Ivao {
             .setTimestamp();
 
         try {
-            let ivaoClient = await this._ivaoManager.getClientInfo(callSign, type);
+            let ivaoClient = this._ivaoManager.getClientInfo(callSign, type);
             ivaoEmbed.setTitle(`IVAO : ${callSign} (open on Webeye)`).setURL(`https://webeye.ivao.aero/?callsign=${ivaoClient.callsign}`);
+            ivaoEmbed.addFields({
+                    name: "Call Sign",
+                    value: ivaoClient.callsign,
+                    inline: true
+                },
+                {
+                    name: "VID",
+                    value: ivaoClient.userId.toString(),
+                    inline: true
+                },
+                {
+                    name: "Rating",
+                    value: ivaoClient.rating.toString(),
+                    inline: true
+                });
             switch (type) {
                 case "pilot":
                     ivaoClient = ivaoClient as IvaoPilot;
                     ivaoEmbed.addFields(
                         {
-                            name: "Call Sign",
-                            value: ivaoClient.callsign,
-                            inline: true
-                        },
-                        {
-                            name: "VID",
-                            value: ivaoClient.vid,
-                            inline: true
-                        },
-                        {
-                            name: "Rating",
-                            value: ivaoClient.atcPilotRating,
-                            inline: true
-                        },
-                        {
                             name: "Departure",
-                            value: ivaoClient.departureAerodrome,
+                            value: ivaoClient.flightPlan.departureId,
                             inline: true
                         },
                         {
                             name: "Destination",
-                            value: ivaoClient.destinationAerodrome,
+                            value: ivaoClient.flightPlan.arrivalId,
                             inline: true
                         },
                         {
                             name: "Transponder",
-                            value: ivaoClient.transponderCode,
+                            value: ivaoClient.lastTrack.transponder.toString(),
                             inline: true
                         },
                         {
                             name: "Latitude",
-                            value: ivaoClient.lastTrack.latitude,
+                            value: ivaoClient.lastTrack.latitude.toString(),
                             inline: true
                         },
                         {
                             name: "Longitude",
-                            value: ivaoClient.longitude,
+                            value: ivaoClient.lastTrack.longitude.toString(),
                             inline: true
                         },
                         {
                             name: "Altitude",
-                            value: ivaoClient.altitude,
+                            value: `${ivaoClient.lastTrack.altitude.toString()} ft`,
                             inline: true
                         },
                         {
                             name: "Ground Speed",
-                            value: ivaoClient.groundSpeed,
+                            value: `${ivaoClient.lastTrack.groundSpeed.toString()} knots`,
                             inline: true
                         },
                         {
                             name: "Cruising Speed",
-                            value: ivaoClient.cruisingSpeed,
+                            value: ivaoClient.flightPlan.speed.toString(),
                             inline: true
                         },
                         {
                             name: "Cruising Level",
-                            value: ivaoClient.cruisingLevel,
+                            value: ivaoClient.flightPlan.level,
                             inline: true
                         },
                         {
                             name: "Departure Time",
-                            value: ivaoClient.departureTime,
+                            value: this.parseTime(ivaoClient.flightPlan.departureTime),
                             inline: true
                         },
                         {
                             name: "EET",
-                            value: `${ivaoClient.eetHours}:${ivaoClient.eetMinutes}`,
+                            value: this.parseTime(ivaoClient.flightPlan.eet),
                             inline: true
                         },
                         {
                             name: "Aircraft",
-                            value: ivaoClient.aircraft.split("/")[1],
+                            value: ivaoClient.flightPlan.aircraftId,
                             inline: true
                         },
                         {
                             name: "Route",
-                            value: ivaoClient.route,
+                            value: ivaoClient.flightPlan.route,
                             inline: true
                         }
                     );
                     break;
                 case "atc":
+                    ivaoClient = ivaoClient as IvaoAtc;
+                    ivaoEmbed.addFields(
+                        {
+                            name: "Position",
+                            value: ivaoClient.atcSession.position,
+                            inline: true
+                        },
+                        {
+                            name: "Frequency",
+                            value: ivaoClient.atcSession.frequency.toString(),
+                            inline: true
+                        },
+                        /*   {
+                              name: "ATIS",
+                              value: ivaoClient.atis,
+                              inline: true
+                          } */);
                     break;
             }
         } catch (e) {
-
+            logger.error(`[${client.shard.ids}] ${e}`);
+            ivaoEmbed.setColor("#ff0000").setDescription(`${interaction.member}, ${e.message}`);
         }
+        return InteractionUtils.replyOrFollowUp(interaction, {
+            embeds: [ivaoEmbed]
+        });
+    }
+
+    @Slash("ivao-online", {
+        description: "Gives you the information for all ATCs which match the given partial callsign on the IVAO network"
+    })
+    @Guard(NotBot, RequiredBotPerms({
+        textChannel: ["EMBED_LINKS"]
+    }), GuildOnly)
+    private ivaoOnline(
+        @SlashOption("partial-callsign", {
+            description: "What partial callsign would you like the bot to give information for?",
+            type: "STRING",
+            required: true
+        })
+            partialCallSign: string,
+        interaction: CommandInteraction,
+        client: Client
+    ): Promise<void> {
+        const ivaoEmbed = new MessageEmbed()
+            .setTitle(`${partialCallSign.toUpperCase()}`)
+            .setColor("#0099ff")
+            .setFooter({
+                text: `${client.user.username} • This is not a source for official briefing • Please use the appropriate forums • Source: IVAO API`
+            })
+            .setTimestamp();
+
+        try {
+            const atcList = this._ivaoManager.getPartialAtcClientInfo(partialCallSign);
+
+            ivaoEmbed.setTitle(`IVAO : ${partialCallSign}`);
+
+            for (const atc of atcList) {
+                ivaoEmbed.addField(`${atc.callsign}`, `VID: ${atc.userId}, Frequency: ${atc.atcSession.frequency}`);
+            }
+
+        } catch (error) {
+            logger.error(`[${client.shard.ids}] ${error}`);
+            ivaoEmbed.setColor("#ff0000").setDescription(`${interaction.member}, ${error.message}`);
+        }
+        return InteractionUtils.replyOrFollowUp(interaction, {
+            embeds: [ivaoEmbed]
+        });
+    }
+
+    private parseTime(time: number): string {
+        return ObjectUtil.dayJs.utc().startOf("day").add(time, "seconds").format("HH:mm Z");
     }
 }
