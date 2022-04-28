44f9753c1ed31ad08b4537ff57ced4aac71e1d87
VictoriqueMoe <loli@victorique.moe>
2022-04-28 11:11:25 +0100

    merge json

--- src/model/framework/manager/IvaoManager.ts
+++ src/model/framework/manager/IvaoManager.ts
@@ -1,12 +1,14 @@
+import axios from "axios";
 import {singleton} from "tsyringe";
 
 import METHOD_EXECUTOR_TIME_UNIT from "../../../enums/METHOD_EXECUTOR_TIME_UNIT.js";
+import logger from "../../../utils/LoggerFactory.js";
+import type {FlightSimNetwork, IvaoAtis, IvaoInfo} from "../../Typeings";
 import {RunEvery} from "../decorators/RunEvery.js";
 import {AbstractCallSignInformationManager} from "./AbstractCallSignInformationManager.js";
-import type {FlightSimNetwork} from "../../Typeings";
 
 @singleton()
-export class IvaoManager extends AbstractCallSignInformationManager {
+export class IvaoManager extends AbstractCallSignInformationManager<IvaoInfo> {
 
     public constructor() {
         super("https://api.ivao.aero/v2/tracker/whazzup");
@@ -19,6 +21,27 @@ export class IvaoManager extends AbstractCallSignInformationManager {
 
     protected get type(): FlightSimNetwork {
         return "Ivao";
+    }
+
+    protected async getData(): Promise<IvaoInfo> {
+        const info = await this.api.get(null);
+        const atisInfo = await axios.get("https://api.ivao.aero/v2/tracker/whazzup/atis");
+        if (info.status !== 200 || atisInfo.status !== 200) {
+            if (info.status !== 200) {
+                logger.error(`[x] ${info.statusText}`);
+            }
+            if (atisInfo.status !== 200) {
+                logger.error(`[x] ${atisInfo.statusText}`);
+            }
+            throw new Error(`Unable to download ${this.type} info`);
+        }
+        const mainInfo: IvaoInfo = info.data;
+        const atisData: IvaoAtis[] = atisInfo.data;
+        const {atcs} = mainInfo.clients;
+        for (const atc of atcs) {
+            atc.atis = atisData.find(atis => atis.callsign === atc.callsign);
+        }
+        return mainInfo;
     }
 
 }
--- src/model/framework/manager/VatsimManager.ts
+++ src/model/framework/manager/VatsimManager.ts
@@ -1,12 +1,13 @@
 import {singleton} from "tsyringe";
 
 import METHOD_EXECUTOR_TIME_UNIT from "../../../enums/METHOD_EXECUTOR_TIME_UNIT.js";
+import logger from "../../../utils/LoggerFactory.js";
+import type {FlightSimNetwork, VatsimInfo} from "../../Typeings";
 import {RunEvery} from "../decorators/RunEvery.js";
 import {AbstractCallSignInformationManager} from "./AbstractCallSignInformationManager.js";
-import type {FlightSimNetwork} from "../../Typeings";
 
 @singleton()
-export class VatsimManager extends AbstractCallSignInformationManager {
+export class VatsimManager extends AbstractCallSignInformationManager<VatsimInfo> {
 
     public constructor() {
         super("https://data.vatsim.net/v3/vatsim-data.json");
@@ -19,6 +20,15 @@ export class VatsimManager extends AbstractCallSignInformationManager {
 
     protected get type(): FlightSimNetwork {
         return "Vatsim";
+    }
+
+    protected async getData(): Promise<VatsimInfo> {
+        const info = await this.api.get(null);
+        if (info.status !== 200) {
+            logger.error(`[x] ${info.statusText}`);
+            throw new Error(`Unable to download ${this.type} info`);
+        }
+        return info.data;
     }
 
 }
