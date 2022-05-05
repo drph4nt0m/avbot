c38e5bb86b1f351708a9d1d7fe0752b7f168d219
Rahul Singh <rahul.singh12221998@gmail.com>
2022-05-05 12:08:18 +0530

    moved server port to property

--- src/api/BotServer.ts
+++ src/api/BotServer.ts
@@ -5,10 +5,14 @@ import type * as http from "http";
 import { container, singleton } from "tsyringe";
 
 import { PostConstruct } from "../model/framework/decorators/PostConstruct.js";
+import { Property } from "../model/framework/decorators/Property.js";
 import logger from "../utils/LoggerFactory.js";
 
 @singleton()
 export class BotServer extends Server {
+    @Property("API_SERVER_PORT")
+    private static readonly port: number;
+
     private readonly classesToLoad = `${dirname(import.meta.url)}/controllers/**/*.{ts,js}`;
 
     constructor() {
@@ -44,6 +48,6 @@ export class BotServer extends Server {
             super.addControllers(instance);
             logger.info(`load ${moduleKey}`);
         }
-        this._server = this.start(4401);
+        this._server = this.start(BotServer.port);
     }
 }
