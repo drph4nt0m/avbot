86b2af4c4288cf3fc5cc45b3f027e1e32cad24fc
Rahul Singh <rahul.singh12221998@gmail.com>
2022-05-22 01:09:09 +0530

    update custom message for flight ratelimit

--- src/commands/Flight.ts
+++ src/commands/Flight.ts
@@ -31,7 +31,7 @@ export class Flight {
     @Guard(
         GuildOnly,
         PremiumGuild,
-        RateLimit(TIME_UNIT.seconds, 120, `Your command is being rate limited! Try again after 2 minutes.`),
+        RateLimit(TIME_UNIT.seconds, 90, `Your command is being rate limited! Try again after {until}.`),
         RequiredBotPerms({
             textChannel: ["EMBED_LINKS"]
         })
