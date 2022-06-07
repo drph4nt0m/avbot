bdefc3787e2ebd646d3ef1225a4f81a583cbb184
Rahul Singh <rahul.singh12221998@gmail.com>
2022-06-07 23:51:34 +0530

    update ratelimit message to ephemeral

--- src/commands/Flight.ts
+++ src/commands/Flight.ts
@@ -31,7 +31,7 @@ export class Flight {
     @Guard(
         GuildOnly,
         PremiumGuild,
-        RateLimit(TIME_UNIT.seconds, 90, `Your command is being rate limited! Try again after {until}.`),
+        RateLimit(TIME_UNIT.seconds, 90, { message: `Your command is being rate limited! Try again after {until}.`, ephemeral: true }),
         RequiredBotPerms({
             textChannel: ["EMBED_LINKS"]
         })
