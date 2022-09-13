5d2e31805bc34a087d51fcb62401c3eec7fbcc31
Victorique <loli@victorique.moe>
2022-09-13 19:21:33 +0100

    update all deps (#326)

--- src/model/framework/manager/PropertyResolutionManager.ts
+++ src/model/framework/manager/PropertyResolutionManager.ts
@@ -1,6 +1,6 @@
 import { singleton } from "tsyringe";
 
-import type { Property } from "../engine/IPropertyResolutionEngine.js";
+import type { PropertyTYpe } from "../engine/IPropertyResolutionEngine.js";
 import { PropertyResolutionFactory } from "../factory/impl/PropertyResolutionFactory.js";
 
 @singleton()
@@ -13,10 +13,10 @@ export class PropertyResolutionManager {
     /**
      * Get system property
      * @param {string} prop
-     * @returns {Property}
+     * @returns {PropertyTYpe}
      */
-    public getProperty(prop: string): Property {
-        let propValue: Property = null;
+    public getProperty(prop: string): PropertyTYpe {
+        let propValue: PropertyTYpe = null;
         for (const resolutionEngine of this._propertyResolutionFactory.engines) {
             const resolvedProp = resolutionEngine.getProperty(prop);
             if (resolvedProp !== null) {
